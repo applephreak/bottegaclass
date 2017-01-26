0f60d984148402002f52b0536aa42a5aaf4d5358
AustynT <Austyntravis9@gmail.com>
2017-01-26 14:50:44 -0700

    updated portfolio to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,6 +9,13 @@ validates_presence_of :title, :body, :main_image, :thumb_image
 def self.angular
   where(subtitle: 'Angular')
 end
+
+
+
+def self.by_position
+  order("position ASC")
+end
+
 scope :ruby_on_rails_porrfolio_items, -> { where(subtitle: 'Ruby on Rails')}
 
 after_initialize :set_defaults
